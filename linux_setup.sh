00164233c5b08b41217190776e31e8648197b839
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-06-08 23:58:16 -0400

    Create linux_setup.sh

---
+++ linux_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt-get install git
+
+# Setting up .vimrc
+git clone https://github.com/amix/vimrc.git
+cd vimrc
+bash install_basic_vimrc.sh
+
+# Setting up .bashrc
+# TODO
+
+echo "Setup Complete."
